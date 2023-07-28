0c34307f2c581e5183f2d822a25812347a0d7cf3
BasiliscX <113856247+BasiliscX@users.noreply.github.com>
2023-07-28 20:05:04 -0300

    V1
    Now you can test the functions in the .cpp file. Add a description for each function.
    ENJOY!!

--- ventanimation.h
+++ ventanimation.h
@@ -4,16 +4,65 @@
 #include<ctime>
 #include<windows.h>
 
-/// Change the cursor's position:
-void PosXY(int x, int y){
+/// Position (X - Y):
+void PosXY(int,int); // Change the cursor's position.
+int  PosX();         // Return position X.
+int  PosY();         // Return position Y.
+void Pos_X_Y_INFO(); // Prints the current X - Y position on the screen.
+/// Console tools:
+int  ConsoleColumns();            // Return the amount of columns.
+int  ConsoleRows();               // Return the amount of rows.
+void ConsoleSize(int,int);        // Change the console size.
+void ConsoleTitle(const char*);   // Change the console title.
+void ConsoleCursorSHOW_HIDE(bool);// Show/hide cursor.
+void ConsoleClsX_Y(int,int,int);  // Clear console(specific row/position).
+void ConsoleDLT();                // Clear all.
+/// Animation functions:
+void TextAnimation(const char*,int);   // Text with animation.
+void BoxAnimation(int,int,int,int);    // Draw a box with animation.
+void Pause(int);                       // Slow down the screen printing.
+void BackgroundAndTextColor(const int);// Set another color to the text with an underline.
+
+/**
+ * Description:
+ * Set a new cursor position based on the X-Y coordinates.
+ * @param x = Receive the new column position.
+ * @param y = Receive the new row position.
+ */void PosXY(int x, int y){
     HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
     COORD newPosition;
     newPosition.X=x;
     newPosition.Y=y;
     SetConsoleCursorPosition(hConsole, newPosition);
 }
-/// cout position (X,Y) of cursor:
-void Pos_X_Y_INFO(){
+/**
+ * Description:
+ * Return the current position X.
+ */int PosX(){
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    if (GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
+        int x=consoleInfo.dwCursorPosition.X;
+        return x;
+    }
+    else return 0;
+}
+/**
+ * Description:
+ * Return the current position Y.
+ */int PosY(){
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    if (GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
+        int y=consoleInfo.dwCursorPosition.Y;
+        return y;
+    }
+    else return 0;
+}
+/**
+ * Description:
+ * Prints the current X - Y position on the screen.
+ */void Pos_X_Y_INFO(){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
@@ -21,42 +70,101 @@ void Pos_X_Y_INFO(){
     COORD posicionCursor = consoleInfo.dwCursorPosition;
     std::cout << "X: " << posicionCursor.X << " - Y: " << posicionCursor.Y << std::endl;
 }
-/// Return position X:
-int PosX(){
-HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-if (GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
-    int x=consoleInfo.dwCursorPosition.X;
-    return x;
+/**
+ * Description:
+ * Return the amount of columns.
+ */int ConsoleColumns(){
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    if(GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
+        int columns=consoleInfo.srWindow.Right-consoleInfo.srWindow.Left+1;
+        return columns;
+        }
+        else return 0;
 }
-else return 0;
+/**
+ * Description:
+ * Return the amount of rows.
+ */int ConsoleRows(){
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    if(GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
+        int rows=consoleInfo.srWindow.Bottom-consoleInfo.srWindow.Top+1;
+        return rows;
+        }
+        else return 0;
 }
-/// Return position Y:
-int PosY(){
-HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-if (GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
-    int y=consoleInfo.dwCursorPosition.Y;
-    return y;
+/**
+ * Description:
+ * Change the console size.
+ * @param X = Receives the new number of columns.
+ * @param Y = Receives the new number of rows.
+ */void ConsoleSize(int X,int Y){
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    SMALL_RECT window ={0,0,X,Y};
+    SetConsoleWindowInfo(hConsole,TRUE,&window);
 }
-else return 0;
+/**
+ * Description:
+ * Change the console title.
+ * @param TITLE = Receives the new title ("New Title." by default).
+ */void ConsoleTitle(const char*TITLE="New Title."){
+    SetConsoleTitle(TEXT(TITLE));
 }
+/**
+ * Description:
+ * Show/hide cursor.
+ * @param CURSOR = Receives 0/FALSE or 1/TRUE (TRUE by default).
+ */void ConsoleCursorSHOW_HIDE(bool CURSOR=true){
 
-/// Slow down the screen printing:
-void Pause(int millisecond) {
-    clock_t endPause=clock()+millisecond*CLOCKS_PER_SEC/1000;
-    while(clock()<endPause){}
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(hConsole, &cursorInfo);
+    cursorInfo.bVisible = CURSOR;
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
 }
+/**
+ * Description:
+ * You can clear the console output by specifying from a single position (X/Y) to a whole line.
+ * @param X = Receives the column position.
+ * @param Y = Receives the row position.
+ * @param amount = Receives the number of columns to delete from X, Y (The entire row by default).
+ */void ConsoleClsX_Y(int X, int Y, int amount=ConsoleColumns()){
+    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord={static_cast<SHORT>(X),static_cast<SHORT>(Y)};
+    DWORD cells=amount;
+    DWORD CHARACTER_W;
 
-/// Draw a box:
-void BoxAnimation(int tipe=1, int width=20, int height=10, int velocity=50){
+    FillConsoleOutputCharacter(hConsole,TEXT(' '),cells,coord,&CHARACTER_W);
+}
+/**
+* Delete all of the console.
+*/void ConsoleDLT(){for(int i=0;i<ConsoleRows();i++)ConsoleClsX_Y(0,i);}
 
-int i, x, y;
-x=PosX();
-y=PosY();
+/**
+ * Description:
+ * You can create a typing animation by entering a text as the first parameter and the typing speed as the second parameter.
+ * @param text = Receives the text ("Here is your text." by default).
+ * @param velocity = Receives the typing speed (10 by default).
+ */void TextAnimation(const char*text="Here is your text.", int velocity=10){
+    int i=0;
+    while(text[i]!='\0'){std::cout<<text[i++];Pause(velocity);}
+}
+/**
+ * Description:
+ * Allows creating a box animation (3 different types).
+ * @param tipe = Receives the box type (1 by default).
+ * @param width = Receives the width of the box (20 by default).
+ * @param width = Receives the height of the box (10 by default).
+ * @param velocity = Receives the print speed (10 by default).
+ */void BoxAnimation(int tipe=1, int width=20, int height=10, int velocity=10){
+    int i, x, y;
+    x=PosX();
+    y=PosY();
 
-switch(tipe){
-case 1:
+    switch(tipe){
+    case 1:
         enum Line1{
                     topLeftCorner1   =218, topRightCorner1   =191,
                     horizontalLine1  =196, verticalLine1     =179,
@@ -73,8 +181,8 @@ case 1:
         std::cout<<(char)bottomLeftCorner1;
         for(i=1;i<width;i++){std::cout<<(char)horizontalLine1;Pause(velocity);}
         std::cout<<(char)bottomRightCorner1;
-break;
-case 2:
+    break;
+    case 2:
         enum Line2{
                     topLeftCorner2   =201, topRightCorner2   =187,
                     horizontalLine2  =205, verticalLine2     =186,
@@ -91,8 +199,8 @@ case 2:
         std::cout<<(char)bottomLeftCorner2;
         for(i=1;i<width;i++){std::cout<<(char)horizontalLine2;Pause(velocity);}
         std::cout<<(char)bottomRightCorner2;
-break;
-case 3:
+    break;
+    case 3:
         enum Line3{
                     topHorizontalLine3   =220,
                     bottomHorizontalLine3=223,
@@ -105,76 +213,29 @@ case 3:
 
         PosXY(x,y);
         for(i=0;i<=width;i++){std::cout<<(char)bottomHorizontalLine3;Pause(velocity);}
-break;
-}
-}
-/// Create a typing animation with the entered text.
-void TextAnimation(const char*text="Here is your text.", int velocity=10){
-
-int i=0;
-while(text[i]!='\0'){std::cout<<text[i++];Pause(velocity);}
-
-}
-/// Set another color to the text with an underline.
-void BackgroundAndTextColor(const int CUSTOM_COLOR=15){
-/*
-<0 = DEFAULT
- 0 = Black      1 = Blue    2 = Green   3 = Cyan    4 = Red
- 5 = Purple     6 = Yellow  7 = White   8 = Gray    9 = Light Blue
-10 = Light Green    11 = Light Cyan     12 = Light Red
-13 = Light Purple   14 = Light Yellow   15 = Bright White
-*/
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-SetConsoleTextAttribute(hConsole, CUSTOM_COLOR | FOREGROUND_INTENSITY);
-}
-/// Prints the amount of columns.
-int ConsoleColumns(){
-HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-if(GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
-    int columns=consoleInfo.srWindow.Right-consoleInfo.srWindow.Left+1;
-    return columns;
+    break;
     }
-    else return 0;
 }
-/// Prints the amount of rows.
-int ConsoleRows(){
-HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
-if(GetConsoleScreenBufferInfo(hConsole, &consoleInfo)){
-    int rows=consoleInfo.srWindow.Bottom-consoleInfo.srWindow.Top+1;
-    return rows;
-    }
-    else return 0;
+/**
+ * Description:
+ * You can create a delay in milliseconds during execution.
+ * @param millisecond = Receives the number of milliseconds for the delay.
+ */void Pause(int millisecond){
+    clock_t endPause=clock()+millisecond*CLOCKS_PER_SEC/1000;
+    while(clock()<endPause){}
 }
-/// change the console size.
-void ConsoleSize(int X=80, int Y=40){
-HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
-SMALL_RECT window ={0,0,X,Y};
-SetConsoleWindowInfo(hConsole,TRUE,&window);
-}
-
-void ConsoleTitle(const char*TITLE="New Title."){
-SetConsoleTitle(TEXT(TITLE));
-}
-/// Clear console(especific line/position).
-void ConsoleClsX_Y(int X, int Y, int amount=ConsoleColumns()){
-    HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD coord={static_cast<SHORT>(X),static_cast<SHORT>(Y)};
-    DWORD cells=amount;
-    DWORD CHARACTER_W;
-
-    FillConsoleOutputCharacter(hConsole,TEXT(' '),cells,coord,&CHARACTER_W);
-}
-/// Show or hide cursor.
-void ConsoleCursorSHOW_HIDE(bool CURSOR=true){
-
+/**
+ * Description:
+ * Set another color to the text with an underline.
+ * @param CUSTOM_COLOR = Receives the number of the color (15 by default).
+ */void BackgroundAndTextColor(const int CUSTOM_COLOR=15){
+        /*
+        <0 = DEFAULT
+         0 = Black      1 = Blue    2 = Green   3 = Cyan    4 = Red
+         5 = Purple     6 = Yellow  7 = White   8 = Gray    9 = Light Blue
+        10 = Light Green    11 = Light Cyan     12 = Light Red
+        13 = Light Purple   14 = Light Yellow   15 = Bright White
+        */
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO cursorInfo;
-
-    GetConsoleCursorInfo(hConsole, &cursorInfo);
-    cursorInfo.bVisible = CURSOR;
-    SetConsoleCursorInfo(hConsole, &cursorInfo);
+    SetConsoleTextAttribute(hConsole, CUSTOM_COLOR | FOREGROUND_INTENSITY);
 }
-
-
--- ventanimationTEST.cpp
+++ ventanimationTEST.cpp
@@ -1,38 +1,107 @@
 #include<iostream>
+#include<string>
 #include"ventanimation.h"
 
 int main(){
-BackgroundAndTextColor(3);
-PosXY(5,1);
-BoxAnimation(1);
-PosXY(PosX(),1);
-BoxAnimation(2);
-PosXY(PosX(),1);
-BoxAnimation(3);
-Pos_X_Y_INFO();
-/*
-cout<<"\nColumnas: "<<ConsoleColumns()<<endl;
-cout<<"Filas: "<<ConsoleRows()<<endl;
 
-int x,y;
-cout<<"Cambiar filas y columnas:\n";
-cin>>x;
-cin>>y;
-ConsoleSize(x,y);
-cout<<ConsoleColumns();
-cout<<ConsoleRows();
-*/
+    int8_t pin=16 ,arrow=27;
 
-ConsoleCursorSHOW_HIDE(0);
+        std::cout<<pin<<" Position (X - Y) FUNTIONS:\n";
+        std::cout<<"\no PosXY(int,int) = Change the cursor's position.\n";
+        std::cout<<arrow<<"current (X=0, Y=4).";
 
-ConsoleClsX_Y(0,2);
+    PosXY(25,5);
+        std::cout<<arrow<<"now (X=25, Y=5).";
+    PosXY(5,7);
+        std::cout<<"o PosX() = Return position X: "<<PosX();
+    PosXY(5,8);        std::cout<<"o PosY() = Return position y: "<<PosY();
+    PosXY(PosX()-30,PosY()+2);
+        std::cout<<"Now you can set the position arithmetically!: PosXY(PosX()-30,PosY()+2).";
+    PosXY(5,PosY()+2);
+        std::cout<<"o Pos_X_Y_INFO() = Prints the current X - Y position on the screen: ";
+        Pos_X_Y_INFO();
 
-ConsoleTitle();
-PosXY(5,PosY()+2);
-BackgroundAndTextColor();
-TextAnimation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec sapien vitae mi fringilla facilisis. Praesent rhoncus condimentum mi vel fermentum. Nam dictum augue a tellus vehicula, eu euismod enim porttitor. Duis id metus et nisi porttitor auctor. Sed dictum, felis eget malesuada tincidunt, elit arcu interdum lectus, eu scelerisque lorem nisi quis ex. Nunc nec bibendum nisi. Suspendisse at leo vitae quam luctus fringilla. Nam aliquet risus vel odio tincidunt, vel rhoncus massa blandit. Vestibulum et velit eu urna fermentum cursus.");
+    PosXY(0,PosY()+1);
+        std::cout<<pin<<" Console tools:";
+    PosXY(5,PosY()+2);
+        std::cout<<"ConsoleColumns() = Return the amount of columns: "<<ConsoleColumns();
+    PosXY(5,PosY()+1);
+        std::cout<<"ConsoleRows() = Return the amount of rows: "<<ConsoleRows();
+    PosXY(5,PosY()+1);
+        std::cout<<"ConsoleSize(int,int) = Change the console size.";
+    PosXY(5,PosY()+1);
+        std::cout<<"Do you want to try? (try with 110, 20):";
 
-ConsoleClsX_Y(0,15);
+    int X,Y,z;
+    char K;
+    PosXY(5,PosY()+1);
+        std::cout<<"Columns: ";std::cin>>X;
+    PosXY(PosX()+25,PosY()-1);
+        std::cout<<"Rows: ";std::cin>>Y;
+        ConsoleSize(X,Y);
+    PosXY(5,PosY()+1);
+        std::cout<<"ConsoleTitle(const char*) = Change the console title: ";
+    PosXY(5,PosY()+1);
+        std::cout<<"New title of the console: ventanimation.h";
+        ConsoleTitle("ventanimation.h");
+    PosXY(5,PosY()+1);
+        std::cout<<"o ConsoleCursorSHOW_HIDE(bool) = Show/hide cursor:";
+        ConsoleCursorSHOW_HIDE(0);
+    PosXY(5,PosY()+1);
+        std::cout<<"o ConsoleClsX_Y(int,int,int) = Clear console(specific row/position):";
+    PosXY(5,PosY()+1);
+        std::cout<<"Rows to eliminate: 0, 14 (Console tools:)";
+        ConsoleClsX_Y(0,14);
+    PosXY(5,PosY()+1);
+        std::cout<<"Word to eliminate: 5, 16, 14 (ConsoleColumns)";
+        ConsoleClsX_Y(5,16,14);
+    PosXY(5,PosY()+1);
+        std::cout<<"Eliminate all: Press any key ";std::cin>>K;
+    ConsoleDLT();
+
+    PosXY(0,0);
+        std::cout<<pin<<" Animation functions:";
+    PosXY(5,PosY()+2);
+        std::cout<<"TextAnimation(const char*,int) = Text with animation.";
+    PosXY(5,PosY()+2);
+        TextAnimation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec sapien vitae mi fringilla facilisis. Praesent rhoncus condimentum mi vel fermentum. Nam dictum augue a tellus vehicula, eu euismod enim porttitor. Duis id metus et nisi porttitor auctor. Sed dictum, felis eget malesuada tincidunt, elit arcu interdum lectus, eu scelerisque lorem nisi quis ex. Nunc nec bibendum nisi. Suspendisse at leo vitae quam luctus fringilla. Nam aliquet risus vel odio tincidunt, vel rhoncus massa blandit. Vestibulum et velit eu urna fermentum cursus.");
+    PosXY(5,15);
+        std::cout<<"BoxAnimation(int,int,int,int) = Draw a box with animation.";
+        PosXY(5,1);
+        BoxAnimation(1);
+        PosXY(PosX(),1);
+        BoxAnimation(2);
+        PosXY(PosX(),1);
+        BoxAnimation(3);
+
+    PosXY(5,16);
+        std::cout<<"Eliminate all: Press any key ";std::cin>>K;
+    ConsoleDLT();
+
+    PosXY(5,1);
+        std::cout<<"BackgroundAndTextColor(const int) = Set another color to the text with an underline.";
+    PosXY(5,2);
+        std::cout<<"Set a color (1 / 15): ";std::cin>>z;
+        BackgroundAndTextColor(z);
+
+    ConsoleDLT();
+
+        PosXY(5,1);
+        BoxAnimation(1);
+        PosXY(PosX(),1);
+        BoxAnimation(2);
+        PosXY(PosX(),1);
+        BoxAnimation(3);
+
+    PosXY(5,PosY()+2);
+        std::cout<<"Make a Box:";
+    PosXY(5,PosY()+1);
+        std::cout<<"width: ";std::cin>>X;
+    PosXY(20,PosY()-1);
+        std::cout<<"height: ";std::cin>>Y;
+
+    ConsoleDLT();
+    BoxAnimation(3,X,Y);
 
 return 0;
 }
